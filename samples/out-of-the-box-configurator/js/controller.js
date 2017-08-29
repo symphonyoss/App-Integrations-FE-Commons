d8a7bd1280214bb2763bb15ce9468c6aed6f75f4
campidelli <campidelli@gmail.com>
2017-08-29 14:16:31 -0300

    APP-1209: Adding unauthorized init.

--- samples/out-of-the-box-configurator/js/controller.js
+++ samples/out-of-the-box-configurator/js/controller.js
@@ -1,5 +1,5 @@
 import 'babel-polyfill';
-import { register } from 'symphony-integration-commons';
+import { initUnauthenticatedApp } from 'symphony-integration-commons';
 import config from './config.service';
 import IssueStateEnricher from '../enrichers/issueStateEnricher';
 import IssueAssigned from '../enrichers/issueAssigned';
@@ -9,5 +9,4 @@ import IssueAssigned from '../enrichers/issueAssigned';
 * @param          SYMPHONY          Global SYMPHONY object
 * @param          appTitle          The app title should appear in the title bar
 */
-
-register(SYMPHONY, config, [new IssueStateEnricher(), new IssueAssigned()]);
+initUnauthenticatedApp(config, [new IssueStateEnricher(), new IssueAssigned()]);
